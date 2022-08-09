673cd5ab16258db6545c7097ec66938253012c2c
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-08-09 17:48:47 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -5,7 +5,7 @@ public static void main(String []args)
 System.out.println("The two numbers are:");
 int a=10;
 int b=5;
-System.out.println(a+" "+b);
+System.out.println(a+","+b);
 int sum = a+b;
 int substraction = a-b;
 int multiplication = a*b;
