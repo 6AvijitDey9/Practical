ead3ca04d92b731c26a6dce2745079c53dba063e
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-08-10 15:47:58 +0530

    Add files via upload

---
+++ AreaPerimeterofRectangle.java
@@ -0,0 +1,13 @@
+class AreaPerimeterofRectangle
+{
+public static void main (String ...args)
+{
+int length = 5;
+int width = 6;
+System.out.println ("The length & width of Rectangle is: " + length +","+ width);
+int Area = length * width;
+int Perimeter = 2*(length + width);
+System.out.println ("The area of Rectangle is : " + Area);
+System.out.println ("The Perimeter of Rectangle is : " + Perimeter);
+}
+}
