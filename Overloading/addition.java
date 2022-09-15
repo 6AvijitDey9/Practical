278a560598be44050218303361908271c54bcf2c
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-09-15 12:26:46 +0530

    Add files via upload

---
+++ Overloading/addition.java
@@ -0,0 +1,19 @@
+// Method overloading: Changing data type of argument 
+package Core;
+
+public class addition{ //Class 
+	static int add(int a, int b) { //no of argument 1, add is a method
+		return (a+b);
+	}
+	static double add(double a, double b) { //no of argument 2, add is a method
+		return (a+b);
+	}
+}
+
+ class OverLoading { //main class
+	public static void main(String[] args) {
+       System.out.println("Addition of two int numbers is: " + addition.add(12, 34));
+       System.out.println("Addition of two int numbers is: " + addition.add(23.567, 34.789));
+	}
+
+}
