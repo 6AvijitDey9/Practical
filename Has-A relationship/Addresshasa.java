affc810efb653f79db842098320ad65448710cd8
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-09-15 12:10:22 +0530

    Update Addresshasa.java

--- Has-A relationship/Addresshasa.java
+++ Has-A relationship/Addresshasa.java
@@ -1,8 +1,8 @@
 package Core;
 
-public class Addresshasa {
+public class Addresshasa { //main class
 		String city, state, country, area;
-		public Addresshasa(String city, String state, String country, String area) {
+		public Addresshasa(String city, String state, String country, String area) { //Constructor
 			this.city=city;
 			this.state=state;
 			this.country=country;
