206d55cdaa94e21c4971cb4e181533b0eb4841a3
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-09-15 12:29:57 +0530

    Add files via upload

---
+++ This keyword/Addresshasa.java
@@ -0,0 +1,13 @@
+package Core;
+
+public class Addresshasa {
+		String city, state, country, area;
+		public Addresshasa(String city, String state, String country, String area) {
+			this.city=city;
+			this.state=state;
+			this.country=country;
+			this.area=area;
+		}
+	}
+
+
