1f61b31fefa98df7bdd3c4929d3d3dc14cf63507
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-09-15 12:31:06 +0530

    Update Addresshasa.java

--- This keyword/Addresshasa.java
+++ This keyword/Addresshasa.java
@@ -3,7 +3,7 @@ package Core;
 public class Addresshasa {
 		String city, state, country, area;
 		public Addresshasa(String city, String state, String country, String area) {
-			this.city=city;
+			this.city=city; //Using this keyword here for reference to the current object
 			this.state=state;
 			this.country=country;
 			this.area=area;
