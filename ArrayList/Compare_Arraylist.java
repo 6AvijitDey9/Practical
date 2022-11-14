73d521da09e68f07019751a4cae33058836b6e1f
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-11-14 23:13:56 +0530

    Add files via upload

---
+++ ArrayList/Compare_Arraylist.java
@@ -0,0 +1,25 @@
+package Array_assignment;
+import java.util.*;
+public class Compare_Arraylist {
+
+	public static void main(String[] args) {
+		// Creating ArrayList
+		ArrayList<String> als = new ArrayList<>();
+		List<String> als1 = new ArrayList<String>();
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		// Adding values on ArrayList
+		als.add("First"); als.add("Second"); als.add("Third"); als.add("fourth");
+		als1.add("First"); als1.add("Second"); als1.add("Third"); als1.add("NoInput");
+		al.add(12); al.add(21); al.add(32); al.add(23);	al.add(19);
+		// Using different methods to get boolean outputs
+		System.out.println(als.equals(als1));
+		System.out.println(als.removeAll(als1));
+		System.out.println(als.retainAll(als1));
+		System.out.println(als.contains(als1));
+		System.out.println(als.toString().contentEquals(als1.toString()));
+		
+		System.out.println(al);
+		Collections.sort(al);
+		for(int val : al) {
+			System.out.print(val + " ");
+		}}}
